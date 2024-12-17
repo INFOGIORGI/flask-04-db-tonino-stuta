be328ac59aadb8eaa7d832d96df40208d31af152
tonino-stuta <119683972+tonino-stuta@users.noreply.github.com>
2024-12-17 11:50:11 +0000

    commit1

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
-from flask import Flask
-
+from flask import Flask, render_template
+from flask_mysqldb import MySQL
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+app.config['MYSQL_HOST'] = '138.41.20.102'
+app.config['MYSQL_PORT'] = 53306
+app.config['MYSQL_DB'] = 'w3school'
+app.config['MYSQL_USER'] = 'ospite'
+app.config['MYSQL_PASSWORD'] = 'ospite'
+mysql = MySQL(app)
 
-app.run()
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/prodotti")
+def products():
+    return render_template("products.html")
+app.run(debug = True)
